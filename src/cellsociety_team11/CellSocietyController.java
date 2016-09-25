e81f618a705b44b41143e3165ce4a2f792b8ac98
Jacob Warner <jrw82@duke.edu>
2016-09-24 21:01:10 -0400

    game of life works now

--- src/cellsociety_team11/CellSocietyController.java
+++ src/cellsociety_team11/CellSocietyController.java
@@ -16,39 +16,28 @@ public class CellSocietyController implements SimulationController{
 		{false, false, true, true, false},
 		{false, true, true, false, false},
 		{false, true, false, false, true},
-		{false, true, false, false, true}			
+		{false, true, false, false, true}
 		};
-	
+
 	private static final int FRAMES_PER_SECOND = 4;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
-	
+
 	private MainWindow mainWindow;
 	private GameOfLifeGrid grid;
 	private Timeline timeline;
 	private double simulationSpeed;
-	
+
 	public CellSocietyController(String language){
 		this.mainWindow = new MainWindow(this, language);
 		grid = new GameOfLifeGrid(TEST_INIT_GRID);
-		//testSetGrid();
 		this.mainWindow.setGrid(grid);
-		
+
 	}
-	
-	private void testSetGrid(){
-		for (int i = 0; i < grid.getWidth(); i++){
-			for (int j = 0; j<grid.getHeight(); j++){
-				Coordinates currentCoords = new Coordinates(i, j);
-				GameOfLifeCell currentCell = (GameOfLifeCell) grid.getCell(currentCoords);
-				currentCell.setNewValue(TEST_INIT_GRID[i][j]);
-			}
-		}
-	}
-	
+
 	public Scene getScene(){
 		return mainWindow.getScene();
 	}
-	
+
 	@Override
 	public void startSimulation(){
 		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
@@ -64,10 +53,10 @@ public class CellSocietyController implements SimulationController{
 		grid.nextGrid();
 		//mainWindow.setGrid(grid);
 	}
-	
+
 	@Override
 	public void updateSimulationSpeed(MouseEvent speedUpdated) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
